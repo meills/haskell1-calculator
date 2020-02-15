3e34d57c2fb349d3bc7191e8991b1406628f7805
craigmckenziec <craigmckenzie70@hotmail.co.uk>
2020-02-15 12:08:33 +0000

    Now working for all values

--- Expr.hs
+++ Expr.hs
@@ -10,36 +10,71 @@ type Name = String
 -- add other operations, and variables
 data Expr = Add Expr Expr
           | Val Int
+          | Name [Char]
           | Minus Expr Expr
           | Multiply Expr Expr
           | Division Expr Expr
+          | Abs Expr
+          | Mod Expr Expr
+          | Power Expr Expr
   deriving Show
 
 -- These are the REPL commands - set a variable name to a value, and evaluate
 -- an expression
 data Command = Set Name Expr
              | Eval Expr
+             | Quit
+             | Read Expr
   deriving Show
 
+
+retrieveVar :: [(Name, Int)] -> Name -> Maybe Int 
+retrieveVar [] x = Nothing
+retrieveVar ((a, b) : vs) x =  if a == x
+                               then Just b
+                               else retrieveVar vs x
+ 
 eval :: [(Name, Int)] -> -- Variable name to value mapping
         Expr -> -- Expression to evaluate
         Maybe Int -- Result (if no errors such as missing variables)
 eval vars (Val x) = Just x -- for values, just give the value directly
-eval vars (Add x y) = do {q <-  (eval vars x) ; p <-  (eval vars y); Just (q + p) } -- Just ((eval vars x) + (eval vars y)) --fmap sum $ sequence [eval vars x, eval vars y]  -- return an error (because it's not implemented yet!)
-eval vars (Minus x y) =  do {q <-  (eval vars x) ; p <-  (eval vars y); Just (q - p) }
-eval vars (Multiply x y) = do {q <-  (eval vars x) ; p <-  (eval vars y); Just (q * p) }
-eval vars (Division x y) = do {q <-  (eval vars x) ; p <-  (eval vars y); Just (div q p) }
+eval vars (Name x) = retrieveVar vars x
+                        --Just x
+eval vars (Add x y) = do q <-  (eval vars x)
+                         p <-  (eval vars y)
+                         Just (q + p) -- Just ((eval vars x) + (eval vars y)) --fmap sum $ sequence [eval vars x, eval vars y]  -- return an error (because it's not implemented yet!)
+eval vars (Minus x y) =  do q <-  (eval vars x) 
+                            p <-  (eval vars y)
+                            Just (q - p)
+eval vars (Multiply x y) = do q <-  (eval vars x)
+                              p <-  (eval vars y)
+                              Just (q * p)
+eval vars (Division x y) = do q <-  (eval vars x)
+                              p <-  (eval vars y)
+                              Just (div q p)
+eval vars (Abs x) = do q <-  (eval vars x)
+                       Just (abs q)
+eval vars (Mod x y) = do q <-  (eval vars x)
+                         p <-  (eval vars y)
+                         Just (mod q p ) 
+eval vars (Power x y) = do q <-  (eval vars x)
+                           p <-  (eval vars y)
+                           Just (q ^ p)                        
 
 digitToInt :: Char -> Int
 digitToInt x = fromEnum x - fromEnum '0'
 
 pCommand :: Parser Command
-pCommand = do t <- letter
-              char '='
-              e <- pExpr
-              return (Set [t] e)
-            ||| do e <- pExpr
-                   return (Eval e)
+pCommand = do string "quit"
+              return (Quit)   
+            ||| do t <- identifier
+                   char '='
+                   e <- pExpr
+                   return (Set t e)
+                ||| do e <- pExpr
+                       return (Eval e)
+                    
+                 
 
 pExpr :: Parser Expr
 pExpr = do t <- pTerm
@@ -52,24 +87,33 @@ pExpr = do t <- pTerm
                  ||| return t
 
 pFactor :: Parser Expr
-pFactor = do d <- int
+pFactor = do d <- integer
              return (Val d)
-           ||| do v <- letter
-                  error "Variables not yet implemented"
+           ||| do v <- identifier
+                  return (Name v)
                 ||| do char '('
                        e <- pExpr
                        char ')'
                        return e
+                     ||| do char '|' 
+                            e <- pExpr
+                            char '|'
+                            return (Abs e)
 
 pTerm :: Parser Expr
-pTerm = do w <- space
-           f <- pFactor
-           w <- space
+pTerm = do f <- pFactor
            do char '*'
               t <- pTerm
               return (Multiply f t)
             ||| do char '/'
                    t <- pTerm
                    return (Division f t)
-                 ||| return f
+                 ||| do char '^' 
+                        t <- pTerm
+                        return (Power f t)
+                      ||| do char '%' 
+                             t <- pTerm
+                             return (Mod f t)
+                            ||| return f
 
+                    
--- REPL.hs
+++ REPL.hs
@@ -3,6 +3,8 @@ module REPL where
 import Expr
 import Parsing
 import Data.Typeable
+import Data.Char (isDigit)
+
 
 
 data State = State { vars :: [(Name, Int)],
@@ -16,16 +18,14 @@ initState = State [] []
 -- If it already exists, remove the old value
 updateVars :: Name -> Int -> [(Name, Int)] -> [(Name, Int)]
 updateVars name int [] = [(name, int)]
-updateVars name int ((a, b):vs) = if a == name
-                                  then do dropVar name ((a, b):vs)
-                                          updateVars name int vs
-                                  else updateVars name int vs
+updateVars name int (v:vs) = do let newVars = dropVar name (v:vs)
+                                newVars ++ [(name, int)]
+                                     
 
 -- Return a new set of variables with the given name removed
 dropVar :: Name -> [(Name, Int)] -> [(Name, Int)]
-dropVar name [] = []
-dropVar name ((a, b):vs) | name == a   = dropVar name vs
-                         | otherwise   = (a, b) : dropVar name vs
+dropVar name ((a,b):vs) = filter (\(a,_) -> a /= name) ((a,b):vs)
+
 
 -- Add a command to the command history in the state
 addHistory :: State -> Command -> State
@@ -42,37 +42,67 @@ checkLength request history = do let lengthOf = (length history) -1
 
 historyCheck :: State -> [Char] -> IO ()
 historyCheck st cmd = do if (length cmd) == 1
-                           then do putStrLn "Parse error"
-                                   repl st
-                           else do let request = last cmd
-                                   let requestInt = digitToInt request
-                                   if (checkLength requestInt (history st) == True)
+                         then do putStrLn "Parse error"
+                                 repl st
+                         else if checkDigits (drop 1 cmd) == True 
+                                then do let request = drop 1 cmd
+                                        let requestInt = read request :: Int
+                                        if (checkLength requestInt (history st) == True)
                                              then process st (history st !!(requestInt))
                                              else do putStrLn "Invalid history point"
                                                      repl st
+                         else do putStrLn "History cannot contain non-integers"
+                                 repl st  
+
+checkDigits :: [Char] -> Bool
+checkDigits [] = True
+checkDigits (x:xs) = do if isDigit x == True then checkDigits xs
+                        else False
 
 
 
 
 process :: State -> Command -> IO ()
 process st (Set var e)
-     = do let st' = addHistory st (Set var e)
+     = do --putStrLn (show var)
+          --putStrLn(show (vars st))
+          --putStrLn (show e)
+          case eval (vars st) e of
+               Just n -> do let newVars = updateVars var n (vars st)
+                            let st' = addHistory st (Set var e)
+                            let st'' = State (newVars) (history st')
+                            putStrLn "OK"
+                            repl st''
+               
+               Nothing -> do putStrLn "Variable has not been declared!"
+                             repl st
+         
+
           -- st' should include the variable set to the result of evaluating e
-          repl st'
 process st (Eval e)
      = do --let st' = addHistory st (Eval e)
          -- let Just n = eval (vars st) e
+          putStrLn( show (vars st))
 
           case eval (vars st) e of
-               Just n -> do let [newVars] = updateVars "it" n (vars st)
+               Just n -> do let newVars = updateVars "it" n (vars st)
                             let st' = addHistory st (Eval e)
-                            let st'' = State [(newVars)] (history st')
+                            let st'' = State (newVars) (history st')
                             putStrLn (show n)
                             --print (show (history st'')) --Commented out (for illustration of working variable storage)
                             repl st''
 
-               Nothing -> do putStrLn "Invalid Number!"
+               Nothing -> do putStrLn "Variable has not been declared!"
                              repl st
+
+          -- Print the result of evaluation
+-- terminate program when Quit command is returned
+process st (Quit)
+     = do putStrLn "bye"
+          return ()
+
+process st (Read e)
+     = putStrLn "Reading File"
 
 
 -- Read, Eval, Print Loop
@@ -85,12 +115,10 @@ repl st = do putStr (show (length (history st)) ++ " > ")
              --putStr (show (length (vars st)) ++ " > ")
              inp <- getLine
              if (head inp) == '!' then do {historyCheck st inp}
-             else if inp == "quit"
-                  then do {putStrLn "bye"; return ()}
-                  else case parse pCommand inp of
-                    [(cmd, "")] -> -- Must parse entire input
-                            process st cmd
-                    _ -> do putStrLn "Parse error"
-                            repl st
+             else case parse pCommand inp of
+                [(cmd, "")] -> -- Must parse entire input
+                        process st cmd
+                _ -> do putStrLn "Parse error"
+                        repl st
 
 
